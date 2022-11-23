998329325343d97b5e78c3fe202830bf247b7ff7
AWyszynska <340746@uwr.edy.pl>
2022-11-23 12:25:32 +0100

    lista4

--- lista4/zd.3.py
+++ lista4/zd.3.py
@@ -1,14 +1,11 @@
-a = int(input())
+a = float(input())
 import math
 def sin_radiany_na_stopnie(x):
-    return math.sin(math.radians(x))
+    return (x*180)/math.pi
 def sin_stopnie_na_radiany(x):
-    return math.sin(math.degrees(x))
-def cos_radiany_na_stopnie(x):
-    return math.cos(math.radians(x))
-def cos_stopnie_na_radiany(x):
-    return math.cos(math.degrees(x))
+    return (x*math.pi)/180
+
+
 print("Sinus - stopnie na radiany: ",sin_stopnie_na_radiany(a))
 print("Sinus - radiany na stopnie: ",sin_radiany_na_stopnie(a))
-print("Cosinus - stopnie na radiany: ",cos_stopnie_na_radiany(a))
-print("Cosinus - radiany na stopnie: ",cos_radiany_na_stopnie(a))   
+   
