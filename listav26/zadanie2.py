6a99b7ee96edb27cd71f42b297b0af0f377c6097
AWyszynska <340746@uwr.edy.pl>
2022-12-14 12:09:21 +0100

    hej

--- listav26/zadanie2.py
+++ listav26/zadanie2.py
@@ -7,6 +7,15 @@ def Szyfr_Cezara(x):
         else:
             litery += chr(ord(x[i]) + stala)
     return litery
+def Szyfr_Cezara2(x):
+    litery = ""
+    for i in range(len(x)):
+        if ord(x[i])>122-stala:
+            litery += chr(ord(x[i]) - stala - 26 )
+        else:
+            litery += chr(ord(x[i]) - stala)
+    return litery
 a = input()
 print(Szyfr_Cezara(a))
+print(Szyfr_Cezara2(Szyfr_Cezara(a)))
     
